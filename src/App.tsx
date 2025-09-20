f14cc6ac8dcb008d511709b9ee208bd89a5db0ad
Nicolas Ramiro <nickmonteiroramiro@gmail.com>
2025-09-20 15:46:23 -0300

    Criando a pagina faq e colocando no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import Home from "./pages/home";
 import QuemSomos from "./pages/quemSomos";
+import Faq from "./pages/faq";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/quem-somos" element={<QuemSomos />} />
-          {/* <Route path="/faq" element={<Faq />} /> */}
+          <Route path="/faq" element={<Faq />} /> 
           {/* <Route path="/proposta" element={<Proposta />} /> */}
           {/* <Route path="/contato" element={<Contato />} /> */}
           {/* <Route path="/consulta" element={<Consulta />} /> */}
---
+++ src/components/faqItem.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+interface FaqItemProps {
+  question: string;
+  answer: string;
+}
+
+export default function FaqItem({ question, answer }: FaqItemProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="rounded-md shadow-md overflow-hidden transition w-full">
+      <button
+        className="w-full flex justify-between items-center p-6 text-lg font-semibold text-orange-500"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span>{question}</span>
+        <span className="text-2xl text-orange-500">{isOpen ? "-" : "+"}</span>
+      </button>
+
+      {isOpen && (
+        <div className="bg-neutral-300 px-6 py-4 text-base whitespace-pre-line text-gray-800">
+          {answer}
+        </div>
+      )}
+    </div>
+  );
+}
